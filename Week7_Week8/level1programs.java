fb2f04b8c17c92ec0fa333cb6ece7f21295768be
yn9307 <yn9307srmist.edu.in>
2026-04-04 13:36:45 +0530

    Added dev code

---
+++ Week7_Week8/level1programs.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class level1programs {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = input.nextLine();
+
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reverse);
+
+        if (str.equalsIgnoreCase(reverse)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
+
+        input.close();
+    }
+
+}
